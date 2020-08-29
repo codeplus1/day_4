279a4b005ae247cbd0e793f12a896b9bc0e222fa
sarojyadav <sarojyadav88505@gmail.com>
2020-08-29 04:01:47 -0700

    first commit

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -73,6 +73,126 @@ class Homepage extends StatelessWidget {
               ),
             ],
           ),
+          Row(
+            children: [
+              Expanded(
+                child: Image.network(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgtzllIb1pAeZgTHe-sGYD-ZSX0v3_aq60gg&usqp=CAU",
+                  width: 300,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Define code",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "A computer code is  a program, software, or sequence of instructions on how the data is to be processed",
+                      style: TextStyle(color: Colors.grey.shade600),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Image.network(
+                  "https://brightlineit.com/wp-content/uploads/2017/03/170309-update-hardware.jpg",
+                  width: 300,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Define Hardware",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "All mechanical part of computer is called hardware",
+                      style: TextStyle(color: Colors.grey.shade600),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Image.network(
+                  "https://blog.ipleaders.in/wp-content/uploads/2020/07/types-of-software.png",
+                  width: 300,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Define software",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "the collection of one or more program is called software",
+                      style: TextStyle(color: Colors.grey.shade600),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Image.network(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSRItpuQM2wvXimp2FeBIEPU4s-y0jVoGS9Xg&usqp=CAU",
+                  width: 300,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Define digital world?",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "Digital describes electronic technology that generates, stores, and processes data in terms of two states: positive and non-positive. ... Digital technology is primarily used with new physical communications media, such as satellite and fiber optic transmission.",
+                      style: TextStyle(color: Colors.grey.shade600),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
